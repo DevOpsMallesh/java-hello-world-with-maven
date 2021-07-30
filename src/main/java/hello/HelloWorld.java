d752e98755a0ed72aac1808ec48371fde9d54367
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2021-07-30 08:28:33 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -6,8 +6,14 @@ public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
+	    System.out.println("The current local time is: " + currentTime);
+	    System.out.println("The current local time is: " + currentTime);
+	    System.out.println("The current local time is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
+	    System.out.println(greeter.sayHello());
+	    System.out.println(greeter.sayHello());
+	    System.out.println(greeter.sayHello());
     }
 }
